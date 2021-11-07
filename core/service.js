7aaf8142ee83cc62582d1b6742e0e470858972f2
Luis Lozada <luis.lozad@gmail.com>
2021-11-07 00:21:13 -0500

    Refactored to accept dynamic properties

--- core/service.js
+++ core/service.js
@@ -1,14 +1,19 @@
-exports.respondText = function (req, res) {
+exports.respondText = function ({ req, res }) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('hi');
 }
 
-exports.respondJson = function (req, res) {
+exports.respondJson = function ({ req, res }) {
 	res.setHeader('Content-Type', 'application/json');
 	res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3, 4] }));
 }
 
-exports.respondNotFound = function (req, res) {
+exports.respondNotFound = function ({ req, res }) {
 	res.writeHead(404, { 'Content-Type': 'text/plain' });
 	res.end('Not Found');
+}
+
+exports.respondEcho = function ({ params, req, res }) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('hi ' + params.value);
 }
