0f9ab6579bf74fe074d9ffb761c57d55222671f9
Luis Lozada <luis.lozad@gmail.com>
2021-11-07 00:20:00 -0500

    The inverse of the normalization of a route has been added

--- core/helper.js
+++ core/helper.js
@@ -1,4 +1,6 @@
-function normalizeRoute (route) {
+const Route = require('route-parser');
+
+function normalizeRoute(route) {
 	let routes = route.split('/');
 	routes = normalizeArray(routes);
 
@@ -11,15 +13,62 @@ function normalizeRoute (route) {
 	} else {
 		return route;
 	}
+}
+
+function normalizeReverseRoute(route) {
+	let routes = route.split('/');
+	routes = normalizeArray(routes);
+
+	if (routes.length > 0) {
+	    routes[0] = '/' + routes[0];
+
+	    return routes.join('/');
+	} else {
+		return route;
+	}
 
 }
 
-function normalizeArray (arr) {
+function normalizeArray(arr) {
 	const newArray = arr.filter(r => r.length > 0);
 	return newArray;
 }
 
+function getRoute(routes, url) {
+	const routeCurrent = {
+		route: null,
+		params: null
+	};
+
+	for (let i = 0; i < routes.length; i++) {
+		const route = routes[i].route;
+
+		if (route != '*' && route != '/*') {
+			const routeResult = new Route(route);
+			const match = routeResult.match(url);
+
+			if (match) {
+				routeCurrent.route = routes[i];
+				routeCurrent.params = match;
+			} else {
+				const routeResultFix = new Route(route);
+				const match = routeResult.match(normalizeReverseRoute(url));
+
+				if (match) {
+					routeCurrent.route = routes[i];
+					routeCurrent.params = match;
+				}
+				
+			}			
+		}
+
+	}
+
+	return routeCurrent;
+}
+
 module.exports = {
 	normalizeRoute,
-	normalizeArray
+	normalizeArray,
+	getRoute
 };
