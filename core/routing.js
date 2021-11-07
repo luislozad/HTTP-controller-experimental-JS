0c9834c0badb08bc0d5461a9c6fa7f7fdc69dfa5
Luis Lozada <luis.lozad@gmail.com>
2021-11-07 00:18:22 -0500

    Routing now accepts dynamic parameters

--- core/routing.js
+++ core/routing.js
@@ -1,14 +1,18 @@
 const http = require('./http');
-const { respondText, respondJson, respondNotFound } = require('./service');
+const { respondText, respondJson, respondNotFound, respondEcho } = require('./service');
 
-http.get('/', (req, res) => {
-	return respondText(req, res);
+http.get('/', (props) => {
+	return respondText(props);
 });
 
-http.get('/json', (req, res) => {
-	return respondJson(req, res);
+http.get('/json', (props) => {
+	return respondJson(props);
 });
 
-http.get('*', (req, res) => {
-	return respondNotFound(req, res);
+http.get('/echo?input=:value', (props) => {
+	return respondEcho(props);
+});
+
+http.get('*', (props) => {
+	return respondNotFound(props);
 });
